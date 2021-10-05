fec0093b15a235fdba80f91e65598fb4f24adee5
K0IN <19688162+K0IN@users.noreply.github.com>
2021-10-05 23:37:11 +0200

    Added readme to output

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     version = '0.3',
     description = 'Using Pastebin pastes as if they were files',
     long_description = readme(),
+    long_description_content_type='text/markdown',
     keywords = 'pastebin file file-like',
     url = 'https://github.com/K0IN/pastebin-as-file',
-    long_description_content_type='text/markdown',
     author = 'K0IN',
     author_email = 'thisk0in@gmail.com',
     license = 'MIT',
@@ -19,4 +19,5 @@ setup(
     zip_safe = False,
     install_requires = [
         'requests',
-])
+    ]
+)
